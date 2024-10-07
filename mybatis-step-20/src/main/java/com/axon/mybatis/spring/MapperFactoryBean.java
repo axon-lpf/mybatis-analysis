c8a4884bf723de3c8dbf26921b3b1bc35b6c9efe
rainbow-lpf <rainbow-lpf@outlook.com>
2024-10-07 18:03:18 +0800

    完成整合spring 操作

---
+++ mybatis-step-20/src/main/java/com/axon/mybatis/spring/MapperFactoryBean.java
@@ -0,0 +1,33 @@
+package com.axon.mybatis.spring;
+
+import com.axon.mybatis.session.SqlSessionFactory;
+
+import org.springframework.beans.factory.FactoryBean;
+
+
+public class MapperFactoryBean<T> implements FactoryBean<T> {
+
+
+    private Class<T> mapperInterface;
+    private SqlSessionFactory sqlSessionFactory;
+
+    public MapperFactoryBean(Class<T> mapperInterface, SqlSessionFactory sqlSessionFactory) {
+        this.mapperInterface = mapperInterface;
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    @Override
+    public T getObject() throws Exception {
+        return sqlSessionFactory.openSession().getMapper(mapperInterface);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return mapperInterface;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
--- mybatis-step-20/src/test/java/com/axon/mybatis/ApiTest.java
+++ mybatis-step-20/src/test/java/com/axon/mybatis/ApiTest.java
@@ -10,6 +10,8 @@ import com.axon.mybatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -21,33 +23,13 @@ public class ApiTest {
 
     private Logger logger = LoggerFactory.getLogger(ApiTest.class);
 
-
-
     @Test
-    public void test_queryActivityById() throws IOException {
-        // 1. 从SqlSessionFactory中获取SqlSession
-        Reader reader = Resources.getResourceAsReader("mybatis-config-datasource.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-
-        // 2. 请求对象
-        ActivityDO req = new ActivityDO();
-        req.setActivityId(10004L);
-
-        // 3. 第一组：SqlSession
-        // 3.1 开启 Session
-        SqlSession sqlSession01 = sqlSessionFactory.openSession();
-        // 3.2 获取映射器对象
-        IActivityDao dao01 = sqlSession01.getMapper(IActivityDao.class);
-        logger.info("测试结果01：{}", JSON.toJSONString(dao01.queryActivityById(req)));
-        sqlSession01.close();
-
-        // 4. 第一组：SqlSession
-        // 4.1 开启 Session
-        SqlSession sqlSession02 = sqlSessionFactory.openSession();
-        // 4.2 获取映射器对象
-        IActivityDao dao02 = sqlSession02.getMapper(IActivityDao.class);
-        logger.info("测试结果02：{}", JSON.toJSONString(dao02.queryActivityById(req)));
-        sqlSession02.close();
+    public void test_ClassPathXmlApplicationContext() {
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring-config.xml");
+        IActivityDao dao = beanFactory.getBean("IActivityDao", IActivityDao.class);
+        ActivityDO activityDO = new ActivityDO();
+        activityDO.setActivityId(10005L);
+        ActivityDO res = dao.queryActivityById(activityDO);
+        logger.info("测试结果：{}", JSON.toJSONString(res));
     }
-
 }
